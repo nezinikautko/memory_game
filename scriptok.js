ada1498396577f07cd175a2f26113410e8bd4cd9
nezinikautko <cat_kite12@disroot.org>
2025-01-25 21:41:21 +0200

    added images and code

--- scriptok.js
+++ scriptok.js
@@ -14,3 +14,37 @@ start_btn.addEventListener("click", function(){
        start_btn.style.display = 'none' 
     })
 })
+
+let images = [
+    "./images/img_1.jpg", "./images/img_2.jpg", "./images/img_3.jpg",
+    "./images/img_4.jpg", "./images/img_5.jpg", "./images/img_6.jpg",
+    "./images/img_7.jpg", "./images/img_8.jpg", "./images/img_9.jpg",
+    "./images/img_10.jpg", "./images/img_11.jpg", "./images/img_12.jpg",
+    "./images/img_13.jpg", "./images/img_14.jpg", "./images/img_15.jpg",
+]
+let images_2 = [...images, ...images].sort(() => Math.random()-0.5)
+
+images_2.forEach((img, i) =>{
+    let card = document.createElement("div")
+    card.classList.add("card")
+    card.dataset.id = i
+    
+    let faceup = document.createElement("div")
+    faceup.classList.add("face")
+    
+    let facedown = document.createElement("div")
+    facedown.classList.add("down")
+    facedown.style.backgroundImage = `url(${img})`
+
+    card.appendChild(faceup)
+    card.appendChild(facedown)
+    game_board.appendChild(card)
+})
+
+let turned_cards = []
+let pairs = 0
+
+game_board.addEventListener("click", (event) => {
+    let clicked_cards = event.target.closest(".card")
+    console.log(clicked_cards)
+})
